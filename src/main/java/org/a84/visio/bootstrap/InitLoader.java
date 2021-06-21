7a98d540ba3ff48802583419a1f841b135e28f85
Bypass <a.caldato@digitalbox.fr>
2021-06-21 12:01:02 +0200

    Added lombok config, refactored initLoader.

--- src/main/java/org/a84/visio/bootstrap/InitLoader.java
+++ src/main/java/org/a84/visio/bootstrap/InitLoader.java
@@ -1,5 +1,8 @@
 package org.a84.visio.bootstrap;
 
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.a84.visio.model.Log;
 import org.a84.visio.service.LogDAO;
 import org.a84.visio.model.User;
@@ -12,27 +15,21 @@ import java.util.Date;
 import java.util.List;
 
 @Component
+@Log4j2
+@RequiredArgsConstructor
 public class InitLoader implements CommandLineRunner {
     /**
      * User DAO Service.
      */
-    private final UserDAO userDAO;
+    private final @NonNull UserDAO userDAO;
     /**
      * Log DAO Service.
      */
-    private final LogDAO logDAO;
+    private final @NonNull LogDAO logDAO;
     /**
      * Pass encoder.
      */
-    private final PasswordEncoder passwordEncoder;
-    /**
-     * Constructor.
-     */
-    public InitLoader(UserDAO userDAO, LogDAO logDAO, PasswordEncoder passwordEncoder) {
-        this.userDAO = userDAO;
-        this.logDAO = logDAO;
-        this.passwordEncoder = passwordEncoder;
-    }
+    private final @NonNull PasswordEncoder passwordEncoder;
     /**
      * Init db.
      * @throws Exception - exception
@@ -41,14 +38,14 @@ public class InitLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
         final List<User> users = userDAO.findAll();
         if (users.isEmpty()) {
-            System.out.println("Adding admin");
+            LOGGER.info("Populating database...");
             final DateFormat shortDate = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
             final String admin_pw = passwordEncoder.encode("aaa");
             final User admin = new User("admin", admin_pw, true, "SADMIN", shortDate.format(new Date()));
             final Log log = new Log("initializer", "AJOUT", admin.getUserName(), shortDate.format(new Date()), "BOSS", "SADMIN");
             userDAO.save(admin);
             logDAO.save(log);
-            System.out.println("Done");
+            LOGGER.info("Done.");
         }
     }
 
