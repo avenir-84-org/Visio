cc22e085537ef67a208ea9009afac0c17f13993c
epgfm <etienne.papegnies@gmail.com>
2021-09-28 11:01:12 +0200

    un-hardcoded shit

--- src/main/java/org/a84/visio/controller/user/ModifyUserController.java
+++ src/main/java/org/a84/visio/controller/user/ModifyUserController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.core.env.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.io.IOException;
 import java.security.Principal;
--- src/main/java/org/a84/visio/controller/user/RemoveUserController.java
+++ src/main/java/org/a84/visio/controller/user/RemoveUserController.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.core.env.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.io.IOException;
 import java.security.Principal;
